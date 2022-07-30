b2ed6449b6ece7d44f186765a44fa2dbc1a3c2ac
MaliziaGrimm <49673233+MaliziaGrimm@users.noreply.github.com>
2022-07-30 12:00:34 +0200

    Version 03a

--- setting.py
+++ setting.py
@@ -1,3 +1,3 @@
 Flask_Server_Name = 'localhost:17102'
-Version_Titel = 'ARMTool V0.2d'
-Version_Program = 'Tool Erfassung Abrechnungsdaten §50a mit DATEV Export V 0.2d'
+Version_Titel = 'ARMTool V0.3a'
+Version_Program = 'Tool Erfassung Abrechnungsdaten §50a mit DATEV Export V 0.3a'
